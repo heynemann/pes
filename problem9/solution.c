cba67407cb5dcd9c7718ba944a8e05cfff9b451d
Bernardo Heynemann <heynemann@gmail.com>
2011-07-11 09:23:03 -0300

    Problema 9

---
+++ problem9/solution.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include "../aux.h"
+
+// project euler problem number 9 - http://projecteuler.net/index.php?section=problems&id=9
+// All problem data is copyright of http://projecteuler.net
+
+// Copyright (C) 2011 by Bernardo Heynemann <heynemann@gmail.com>
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+
+int main() {
+    for(int a=0; a < 1000; a++) {
+        for(int b=0; b < 1000; b++) {
+            int c = 1000 - a - b;
+            if (pow(a, 2) == pow(b, 2) + pow(c, 2)) {
+                printf("The numbers are: a=%d, b=%d, c=%d\n", a, b, c);
+                printf("The product is %d\n\n", a * b * c);
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
