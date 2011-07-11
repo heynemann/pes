4b7e8dc7d5f866cbea0363fe95a9635fc32a9fd3
Bernardo Heynemann <heynemann@gmail.com>
2011-07-11 02:32:25 -0300

    problem 4

---
+++ problem4/solution.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// project euler problem number 6 - http://projecteuler.net/index.php?section=problems&id=6
+// All problem data is copyright of http://projecteuler.net
+
+// Copyright (C) 2011 by Bernardo Heynemann <heynemann@gmail.com>
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+
+char* itoa(int value, char* result, int base) {
+    // check that the base if valid
+    if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+    char* ptr = result, *ptr1 = result, tmp_char;
+    int tmp_value;
+
+    do {
+        tmp_value = value;
+        value /= base;
+        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+    } while ( value );
+
+    // Apply negative sign
+    if (tmp_value < 0) *ptr++ = '-';
+    *ptr-- = '\0';
+    while(ptr1 < ptr) {
+        tmp_char = *ptr;
+        *ptr--= *ptr1;
+        *ptr1++ = tmp_char;
+    }
+    return result;
+}
+
+int is_palindrome(int number) {
+    char buf[6];
+    itoa(number, buf, 10);
+    int length = strlen(buf);
+
+    for (int current_length=0; current_length < length; current_length++) {
+        int first = current_length;
+        int last = length - current_length - 1;
+        if (buf[first] != buf[last]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int largest_palindrome() {
+    int max = 0;
+    for (int i=999; i>=100; i--) {
+        for (int j=999; j>=100; j--) {
+            int number = i * j;
+            if (is_palindrome(number) == 1 && number > max) {
+                max = number;
+            }
+        }
+    }
+    return max;
+}
+
+int main() {
+    printf("The largest palindrome is %d", largest_palindrome());
+    printf("\n\n");
+    return 0;
+}
